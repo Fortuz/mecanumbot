5191d7cf524c1f1a88c8046c3c029fa211d90b2c
Fortuz <fortuz19@gmail.com>
2025-08-29 15:47:37 +0200

    deleted colcon trash

--- install/mecanumbot_msgs/share/mecanumbot_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(mecanumbot_msgs_IDL_FILES "msg/SensorState.idl;srv/GetLedStatus.idl;srv/SetLedStatus.idl")
-set(mecanumbot_msgs_INTERFACE_FILES "msg/SensorState.msg;srv/GetLedStatus.srv;srv/GetLedStatus_Request.msg;srv/GetLedStatus_Response.msg;srv/SetLedStatus.srv;srv/SetLedStatus_Request.msg;srv/SetLedStatus_Response.msg")
