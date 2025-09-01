62f03164ca285fed51c66b7f7bf48252151faa2f
Fortuz <fortuz19@gmail.com>
2025-09-01 17:01:36 +0200

    _ros need

--- mecanumbot_bringup/launch/robot.launch.py
+++ mecanumbot_bringup/launch/robot.launch.py
@@ -115,7 +115,7 @@ def generate_launch_description():
 
         Node(
             package='mecanumbot_node',
-            executable='mecanumbot_node',
+            executable='mecanumbot_ros',
             parameters=[
                 tb3_param_dir,
                 {'namespace': namespace}],
