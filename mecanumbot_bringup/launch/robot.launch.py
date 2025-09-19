21b24401ce2118083b3880f6d8c357e73b9c3908
Fortuz <fortuz19@gmail.com>
2025-09-19 11:26:07 +0200

    update robot launch -Csenge

--- mecanumbot_bringup/launch/robot.launch.py
+++ mecanumbot_bringup/launch/robot.launch.py
@@ -113,26 +113,6 @@ def generate_launch_description():
                               'namespace': namespace}.items(),
         ),
         Node(
-            package='nav2_lifecycle_manager',
-            executable='lifecycle_manager',
-            name='lifecycle_manager_map',
-            output='screen',
-            parameters=[{
-                'use_sim_time': use_sim_time,
-                'autostart': True,
-                'node_names': ['map_server']
-            }]
-),
-        Node(
-                package='nav2_map_server',
-                executable='map_server',
-                name='map_server',
-                output='screen',
-                parameters=[{
-                    'yaml_filename': os.path.join(get_package_share_directory('mecanumbot_bringup'), 'map', 'AI_room_lounge.yaml'),
-                }]
-            ),
-        Node(
             package='turtlebot3_node',
             executable='turtlebot3_ros',
             parameters=[
