26120b155c83d786ec0efbc5145c42906f95fb9b
Csenge Hubay <csengehubay@gmail.com>
2025-09-25 08:49:22 +0200

    replace map launch file
    include nav2_bringup with a yaml instead of functions one by one

--- mecanumbot_bringup/launch/map.launch.py
+++ mecanumbot_bringup/launch/map.launch.py
@@ -1,103 +1,33 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-from launch.substitutions import ThisLaunchFileDir
-from launch_ros.actions import Node
-from launch_ros.actions import PushRosNamespace
-
-
-import os
-
-from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
-from launch.substitutions import ThisLaunchFileDir
-from launch_ros.actions import Node
-from launch_ros.actions import PushRosNamespace
 
 
 def generate_launch_description():
-    #TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
-    ROS_DISTRO = os.environ.get('ROS_DISTRO')
-
-    namespace = LaunchConfiguration('namespace', default='')
-
-    usb_port = LaunchConfiguration('usb_port', default='/dev/ttyACM0')
-
-    if ROS_DISTRO == 'humble':
-        tb3_param_dir = LaunchConfiguration(
-            'tb3_param_dir',
-            default=os.path.join(
-                get_package_share_directory('mecanumbot_bringup'),
-                'param',
-                ROS_DISTRO,
-                'mecanumbot.yaml'))
-    else:
-        tb3_param_dir = LaunchConfiguration(
-            'tb3_param_dir',
-            default=os.path.join(
-                get_package_share_directory('mecanumbot_bringup'),
-                'param',
-                'mecanumbot.yaml'))
-
-
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
 
+
+    nav2_bringup_pkg_share = get_package_share_directory('nav2_bringup')
+
+    nav2_bringup_launch_file = os.path.join(
+                                            nav2_bringup_pkg_share,
+                                            'launch',
+                                            'navigation_launch.py'
+                                            )
+    nav2_bringup_launch = IncludeLaunchDescription(
+                                            PythonLaunchDescriptionSource(nav2_bringup_launch_file),
+                                            launch_arguments={
+                                                'use_sim_time': use_sim_time,
+                                                'params_file': os.path.join(
+                                                    get_package_share_directory('mecanumbot_bringup'),
+                                                    'param',
+                                                    'nav2_params.yaml')
+                                                }.items()
+                                            )
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value=use_sim_time,
-            description='Use simulation (Gazebo) clock if true'),
-
-        DeclareLaunchArgument(
-            'usb_port',
-            default_value=usb_port,
-            description='Connected USB port with OpenCR'),
-
-        DeclareLaunchArgument(
-            'tb3_param_dir',
-            default_value=tb3_param_dir,
-            description='Full path to turtlebot3 parameter file to load'),
-
-        DeclareLaunchArgument(
-            'namespace',
-            default_value=namespace,
-            description='Namespace for nodes'),
-
-        PushRosNamespace(namespace),
-    Node(
-            package='nav2_lifecycle_manager',
-            executable='lifecycle_manager',
-            name='lifecycle_manager_map',
-            output='screen',
-            parameters=[{
-                'use_sim_time': use_sim_time,
-                'autostart': True,
-                'node_names': ['map_server','amcl']
-            }]
-        ),
-    Node(
-            package='nav2_amcl',
-            executable='amcl',
-            name='amcl',
-            output='screen',
-            parameters=[os.path.join(get_package_share_directory('mecanumbot_bringup'), 'param','amcl.yaml'), {'use_sim_time': use_sim_time}]
-        ),
-    Node(
-                package='nav2_map_server',
-                executable='map_server',
-                name='map_server',
-                output='screen',
-                parameters=[{
-                    'yaml_filename': os.path.join(get_package_share_directory('mecanumbot_bringup'), 'map', 'AI_room_lounge.yaml'),
-                }]
-            )
+        nav2_bringup_launch
     ])
