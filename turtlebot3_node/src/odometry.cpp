77aba03f5772c6790626adcdb816bcd0f919176d
Fortuz <fortuz19@gmail.com>
2025-10-06 12:41:13 +0200

    init velocity-based odometry calculations - Csenge replace wheel position with velocities for calculation TODO: fine-tune parameters

--- turtlebot3_node/src/odometry.cpp
+++ turtlebot3_node/src/odometry.cpp
@@ -232,17 +232,17 @@ void Odometry::update_joint_state(
   RCLCPP_WARN(nh_->get_logger(), "JointState position array too small (%zu). Skipping update.", joint_state->position.size());
   return;
 }
-  static std::array<double, 4> last_joint_positions = {0.0f, 0.0f, 0.0f, 0.0f};
+  static std::array<double, 4> last_joint_velocities = {0.0f, 0.0f, 0.0f, 0.0f};
 
-  diff_joint_positions_[0] = joint_state->position[0] - last_joint_positions[0]; //FL;3
-  diff_joint_positions_[1] = joint_state->position[1] - last_joint_positions[1]; //FR;4
-  diff_joint_positions_[2] = joint_state->position[2] - last_joint_positions[2]; //RR;1
-  diff_joint_positions_[3] = joint_state->position[3] - last_joint_positions[3]; //RL;2
+  diff_joint_velocities_[0] = joint_state->velocity[0] - last_joint_velocities[0]; //FL;3
+  diff_joint_velocities_[1] = joint_state->velocity[1] - last_joint_velocities[1]; //FR;4
+  diff_joint_velocities_[2] = joint_state->velocity[2] - last_joint_velocities[2]; //RR;1
+  diff_joint_velocities_[3] = joint_state->velocity[3] - last_joint_velocities[3]; //RL;2
 
-  last_joint_positions[0] = joint_state->position[0]; //FL
-  last_joint_positions[1] = joint_state->position[1]; //FR
-  last_joint_positions[2] = joint_state->position[2]; //RR
-  last_joint_positions[3] = joint_state->position[3]; //RL
+  last_joint_velocities[0] = joint_state->velocity[0]; //FL
+  last_joint_velocities[1] = joint_state->velocity[1]; //FR
+  last_joint_velocities[2] = joint_state->velocity[2]; //RR
+  last_joint_velocities[3] = joint_state->velocity[3]; //RL
 }
 
 void Odometry::update_imu(const std::shared_ptr<sensor_msgs::msg::Imu const> &imu)
@@ -256,10 +256,10 @@ bool Odometry::calculate_odometry(const rclcpp::Duration &duration)
 {
 
   // rotation value of wheel [rad]
-  double wheel_fl = diff_joint_positions_[0]; //FL;3
-  double wheel_fr = diff_joint_positions_[1]; //FR;4
-  double wheel_rl = diff_joint_positions_[3]; //RL;1
-  double wheel_rr = diff_joint_positions_[2]; //RR;2
+  double wheel_fl = diff_joint_velocities_[0]; //FL;3
+  double wheel_fr = diff_joint_velocities_[1]; //FR;4
+  double wheel_rl = diff_joint_velocities_[3]; //RL;1
+  double wheel_rr = diff_joint_velocities_[2]; //RR;2
   if (!std::isfinite(wheel_fl) || !std::isfinite(wheel_fr) ||
     !std::isfinite(wheel_rl) || !std::isfinite(wheel_rr)) {
     RCLCPP_WARN(nh_->get_logger(), "NaN or Inf in wheel data. Skipping odometry update.");
@@ -303,8 +303,8 @@ bool Odometry::calculate_odometry(const rclcpp::Duration &duration)
     wheel_rr = 0.0;
   }
 
-  delta_x = wheels_radius_ * (wheel_fl + wheel_fr + wheel_rl + wheel_rr) / 4.0;
-  delta_y = wheels_radius_ * (wheel_fl - wheel_fr - wheel_rl + wheel_rr) / 4.0;
+  delta_x = step_time * (wheel_fl + wheel_fr + wheel_rl + wheel_rr) / 4.0;
+  delta_y = step_time * (wheel_fl - wheel_fr - wheel_rl + wheel_rr) / 4.0;
 
   if (use_imu_)
   {
@@ -327,7 +327,7 @@ bool Odometry::calculate_odometry(const rclcpp::Duration &duration)
   }
   else
   {
-  theta = wheels_radius_ * (-wheel_fl + wheel_fr - wheel_rl + wheel_rr) / (4 * (wheels_separation_y_ + wheels_separation_x_)* 3.14592);
+  theta = step_time *wheels_radius_ * (-wheel_fl + wheel_fr - wheel_rl + wheel_rr) / (4 * (wheels_separation_y_ + wheels_separation_x_)* 3.14592);
   delta_theta = theta;
  // RCLCPP_INFO(nh_->get_logger(), "Odometry, calculated delta_theta : %f", delta_theta);
   }
