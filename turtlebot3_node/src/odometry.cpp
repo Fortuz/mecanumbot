1e2a95f598bc25ca78818f419f10ccf2fdadb5fa
Fortuz <fortuz19@gmail.com>
2025-10-13 11:27:33 +0200

    add debug velocity info

--- turtlebot3_node/src/odometry.cpp
+++ turtlebot3_node/src/odometry.cpp
@@ -337,8 +337,6 @@ bool Odometry::calculate_odometry(const rclcpp::Duration &duration)
   robot_pose_[1] += delta_x * sin(robot_pose_[2]) + delta_y * cos(robot_pose_[2]);
   robot_pose_[2] += delta_theta;
 
-  RCLCPP_DEBUG(nh_->get_logger(), "x : %f, y : %f, theta : %f", robot_pose_[0], robot_pose_[1], robot_pose_[2]);
-  RCLCPP_DEBUG(nh_->get_logger(), "Odometry, delta_x : %f, delta_y : %f, delta_theta : %f, v_x : %f, v_y : %f, w : %f", delta_x, delta_y, delta_theta, v_x, v_y, w);
 
   robot_vel_[0] = v_x;
   robot_vel_[1] = v_y;
@@ -353,6 +351,8 @@ bool Odometry::calculate_odometry(const rclcpp::Duration &duration)
   RCLCPP_WARN(nh_->get_logger(), "Skipping odometry publish due to NaN or Inf values.");
   return false;
   }
+  RCLCPP_INFO(nh_->get_logger(), "Odometry calculated, dx : %f, dy : %f, dtheta : %f, v_x : %f, v_y : %f, w : %f", delta_x, delta_y, delta_theta, v_x, v_y, w);
+  RCLCPP_INFO(nh_->get_logger(), "Odometry, x : %f, y : %f, theta : %f, v_x : %f, v_y : %f, w : %f", robot_pose_[0], robot_pose_[1], robot_pose_[2], robot_vel_[0], robot_vel_[1], robot_vel_[2]);
   last_theta_ = theta;
   return true;
 }
