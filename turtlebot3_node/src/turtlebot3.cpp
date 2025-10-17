de313a8a62d191525bc0ba1dda33c4f3dc8a90a7
Fortuz <fortuz19@gmail.com>
2025-10-17 10:42:57 +0200

    minor bugfix - Csenge

--- turtlebot3_node/src/turtlebot3.cpp
+++ turtlebot3_node/src/turtlebot3.cpp
@@ -69,17 +69,18 @@ void TurtleBot3::init_dynamixel_sdk_wrapper(const std::string & usb_port)
   // ControlItem model_number = {0, EEPROM, 2, READ};
   // ControlItem millis = {10, RAM, 4, READ};
   // ControlItem profile_acceleration_backright = {206, RAM, 4, READ_WRITE};
+  // ControlItem profile_acceleration_present_grabber_right = {234, RAM, 4, READ};
   // 206-10+4 = 200
   // Original 10-178+4 = 10 - 182
-  // Mecanum 10-234+4  = 10 - 228
+  // Mecanum 234 - 10 + 4 = 228
   // Possible: hard coded wrong addresses somewhere else
   // TODO - make sure to check the arduino code once again
   // Control item max: 128 - is it relevant?
   // void init_read_memory(const uint16_t & start_addr, const uint16_t & length);
   dxl_sdk_wrapper_->init_read_memory( // TODO: Check this, probably wrong setting
     extern_control_table.millis.addr,
-    (extern_control_table.profile_acceleration_present_grabber_right.addr - extern_control_table.millis.addr) +
-    extern_control_table.profile_acceleration_present_grabber_right.length
+    (extern_control_table.present_grabber_right_position.addr - extern_control_table.millis.addr) +
+    extern_control_table.present_grabber_right_position .length
   );  //234 - 10 + 4 = 228
 }
 
