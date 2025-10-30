354390df61ecf89801723eb76a6a8ade8c2f2aaf
Fortuz <fortuz19@gmail.com>
2025-10-30 10:23:28 +0100

    change baud rate - Csenge
    Set all baud rates between ROS and OpenCR from 115200 to 57600

--- turtlebot3_node/src/turtlebot3.cpp
+++ turtlebot3_node/src/turtlebot3.cpp
@@ -59,7 +59,7 @@ void TurtleBot3::init_dynamixel_sdk_wrapper(const std::string & usb_port)
   this->declare_parameter<std::string>("namespace");
 
   this->get_parameter_or<uint8_t>("opencr.id", opencr.id, 200);
-  this->get_parameter_or<int>("opencr.baud_rate", opencr.baud_rate, 115200);
+  this->get_parameter_or<int>("opencr.baud_rate", opencr.baud_rate, 57600);
   this->get_parameter_or<float>("opencr.protocol_version", opencr.protocol_version, 2.0f);
 
   RCLCPP_INFO(this->get_logger(), "Init DynamixelSDKWrapper");
