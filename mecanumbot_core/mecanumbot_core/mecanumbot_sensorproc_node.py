82d5a7aec58171595940c4312487b8257ba74744
Fortuz <fortuz19@gmail.com>
2025-11-14 15:55:59 +0100

    Add sensorprocessing node - Csenge
    Create node to core that handles sensors from OpenCR board (IMU, ODOM, BATTSTATE,JOINTSTATE)
    Modify OpenCR sensorstate params to be more clear

---
+++ mecanumbot_core/mecanumbot_core/mecanumbot_sensorproc_node.py
@@ -0,0 +1,196 @@
+import os 
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Imu, JointState, BatteryState
+from geometry_msgs.msg import Twist
+from mecanumbot_msgs.msg import OpenCRState
+import serial
+import struct
+import time
+import math
+
+################################################ MAIN CLASS ################################################
+class Mecanumbot_Sensorproc_Node(Node):
+
+    def __init__(self):
+        super().__init__('mecanumbot_sensorproc_node')
+        self.declare_parameters(
+        namespace='',
+        parameters=[    #TODO
+        
+        ('robot_params.vel_tick', 0.229), # meaning of one tick between velocity values [rot/min]
+        ('robot_params.wheel.radius', 0.0325), # radius [m]
+        ('robot_params.wheel.sep_x',0.129), # distance between front and back wheels [m]
+        ('robot_params.wheel.sep_y',0.300), # distance between left and right wheels [m]
+        ('robot_params.battery.min_voltage',9.9), #minimum voltage of battery [V]
+        ('robot_params.battery.max_voltage',12.6), #maximum voltage of battery [V]
+        ('odom_params.frame_id', 'odom'),
+        ('odom_params.child_frame_id', 'base_footprint'),
+        ('odom_params.from_imu', True),
+        ('imu_params.frame_id', 'imu_link'),
+        
+         ])
+        
+        self.odom_from_imu = self.get_parameter('odom_params.from_imu').value
+        self.vel_tick = self.get_parameter('robot_params.vel_tick').value/60 # rot/min to rot/s
+        self.wheel_radius = self.get_parameter('robot_params.wheel.radius').value # m
+        self.wheel_sep_x = self.get_parameter('robot_params.wheel.sep_x').value # m
+        self.wheel_sep_y = self.get_parameter('robot_params.wheel.sep_y').value
+        self.battery_min_voltage = self.get_parameter('robot_params.battery.min_voltage').value # V
+        self.battery_max_voltage = self.get_parameter('robot_params.battery.max_voltage').value # V
+
+         # Initialize messages
+
+        self.cr_state = OpenCRState()
+        self.odom = Odometry()
+        self.imu = Imu()
+        self.joint_state = JointState()
+        self.battery_state = BatteryState()
+         # Publishers
+
+        self.odom_publisher = self.create_publisher(Odometry, 'mecanumbot/odom', 10)
+        self.imu_publisher = self.create_publisher(Imu, 'mecanumbot/imu', 10)
+        self.joint_state_publisher = self.create_publisher(JointState, 'mecanumbot/joint_states', 10)
+        self.battery_state_publisher = self.create_publisher(BatteryState, 'mecanumbot/battery_state', 10)
+        
+        timer_period = 0.05  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        
+        self.board_subscription = self.create_subscription(OpenCRState, 'mecanumbot/opencr_state', self.crstate_callback, 10)
+        self.board_subscription  # prevent unused variable warning
+
+        self.current_time = self.get_clock().now().nanoseconds()
+        self.last_time = self.get_clock().now().nanoseconds()
+        self.dt = self.current_time - self.last_time
+        
+    def crstate_callback(self,data):
+        self.cr_state = data
+
+    def timer_callback(self):
+        self.last_time = self.current_time
+        self.current_time = self.get_clock().now().nanoseconds()
+        self.dt = (self.current_time - self.last_time) / 1e9 #[s]
+        self.set_odom()
+        self.set_imu()
+        self.set_joint_state()
+        self.set_battery_state()
+
+         # Publish messages
+        self.odom_publisher.publish(self.odom)
+        self.imu_publisher.publish(self.imu)
+        self.joint_state_publisher.publish(self.joint_state)
+        self.battery_state_publisher.publish(self.battery_state)
+
+    def set_odom(self):
+            
+            msg = Odometry()
+            msg.header.stamp = self.current_time.to_msg()
+            msg.header.frame_id = 'odom'
+            msg.child_frame_id = 'base_link'
+
+            Vx_tick = (self.cr_state.vel_bl + self.cr_state.vel_br + self.cr_state.vel_fl + self.cr_state.vel_fr)/4
+            Vy_tick = (-self.cr_state.vel_bl + self.cr_state.vel_br + self.cr_state.vel_fl - self.cr_state.vel_fr)/4
+            Wz_tick = (-self.cr_state.vel_bl + self.cr_state.vel_br - self.cr_state.vel_fl + self.cr_state.vel_fr)/4
+
+
+            dt = (self.current_time - self.last_time).nanoseconds / 1e9
+
+            msg.twist.twist.linear.x = Vx_tick * self.vel_tick * 2 * math.pi * self.wheel_radius  # m/s
+            msg.twist.twist.linear.y = Vy_tick * self.vel_tick * 2 * math.pi * self.wheel_radius  # m/s
+            msg.twist.twist.angular.z = (Wz_tick * self.vel_tick * 2 * math.pi * self.wheel_radius *2) / (self.wheel_sep_x + self.wheel_sep_y)  # rad/s
+            
+            dx =  msg.twist.twist.linear.x * dt
+            dy = msg.twist.twist.linear.y * dt
+            dtheta = msg.twist.twist.angular.z * dt
+
+            msg.pose.position.x = self.odom.pose.pose.position.x + (math.cos(self.odom.pose.pose.orientation.z) * dx - math.sin(self.odom.pose.pose.orientation.z) * dy)
+            msg.pose.position.y = self.odom.pose.pose.position.y + (math.sin(self.odom.pose.pose.orientation.z) * dx + math.cos(self.odom.pose.pose.orientation.z) * dy)
+            msg.pose.pose.position.z = 0.0
+
+            if self.odom_from_imu: #TODO
+                # Orientation from IMU
+                msg.pose.pose.orientation.x = 0.0
+                msg.pose.pose.orientation.y = 0.0
+                msg.pose.pose.orientation.z = self.cr_state.imu_orientation_z
+                msg.pose.pose.orientation.w = self.cr_state.imu_orientation_w
+            else:
+                # Orientation from odometry integration (not implemented)
+                msg.pose.pose.orientation.x = 0.0
+                msg.pose.pose.orientation.y = 0.0
+                msg.pose.pose.orientation.z = self.odom.pose.pose.orientation.z + dtheta  # rad
+                msg.pose.pose.orientation.w = 1.0
+                self.odom = msg
+
+    def set_imu(self):
+            
+            msg = Imu()
+            msg.header.stamp = self.current_time.to_msg()
+            msg.header.frame_id = self.imu_frame_id
+
+            # Fill IMU data from OpenCRState
+            msg.orientation.x = self.cr_state.imu_orientation_x
+            msg.orientation.y = self.cr_state.imu_orientation_y
+            msg.orientation.z = self.cr_state.imu_orientation_z
+            msg.orientation.w = self.cr_state.imu_orientation_w
+            msg.angular_velocity.x = self.cr_state.imu_angular_vel_x
+            msg.angular_velocity.y = self.cr_state.imu_angular_vel_y
+            msg.angular_velocity.z = self.cr_state.imu_angular_vel_z
+            msg.linear_acceleration.x = self.cr_state.imu_linear_acc_x
+            msg.linear_acceleration.y = self.cr_state.imu_linear_acc_y
+            msg.linear_acceleration.z = self.cr_state.imu_linear_acc_z
+
+            self.imu = msg
+
+    def set_joint_state(self):
+            
+            msg = JointState()
+            msg.header.stamp = self.current_time.to_msg()
+            msg.header.frame_id = 'base_link'
+
+            msg.name = ['wheel_backleft_joint', 'wheel_backright_joint', 'wheel_frontleft_joint', 'wheel_frontright_joint']
+            vels = [
+                self.cr_state.vel_bl * self.vel_tick * 2 * math.pi,  # m/s
+                self.cr_state.vel_br * self.vel_tick * 2 * math.pi,  # m/s
+                self.cr_state.vel_fl * self.vel_tick * 2 * math.pi,  # m/s
+                self.cr_state.vel_fr * self.vel_tick * 2 * math.pi   # m/s
+            ]
+
+            posis = [item*self.dt for item in vels]
+            msg.position = posis
+            msg.velocity = vels
+
+            msg.effort = [0.0, 0.0, 0.0, 0.0]  # Effort is not provided by OpenCRState
+
+            self.joint_state = msg
+
+    def set_battery_state(self): #could be more accurate - Temperature. cell values, status. etc.
+    
+            msg = BatteryState()
+            msg.header.stamp = self.current_time.to_msg()
+            msg.voltage = self.cr_state.battery_voltage  # Volts
+            msg.design_capacity = 1.8
+            msg.capacity = 1.8
+            msg.percentage = (self.cr_state.battery_voltage - self.battery_min_voltage) / (self.battery_max_voltage - self.battery_min_voltage)
+            msg.charge = msg.capacity * msg.percentage
+
+            self.battery_state = msg
+    
+def main(args=None):
+    rclpy.init(args=args)
+
+    sensorproc_node = Mecanumbot_Sensorproc_Node()
+
+    rclpy.spin(sensorproc_node)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    sensorproc_node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
