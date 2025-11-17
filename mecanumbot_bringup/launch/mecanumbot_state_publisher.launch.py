62bb777c980d2a2b310dfb5d0ba6137dc706f7dc
Fortuz <fortuz19@gmail.com>
2025-11-17 14:59:51 +0100

    remap joint_states

--- mecanumbot_bringup/launch/mecanumbot_state_publisher.launch.py
+++ mecanumbot_bringup/launch/mecanumbot_state_publisher.launch.py
@@ -52,5 +52,6 @@ def generate_launch_description():
             parameters=[
                     rsp_params,
                     {'use_sim_time': use_sim_time,
-                     'namespace': namespace}])
+                     'namespace': namespace}]),
+            remappings=[('/joint_states', '/mecanumbot/joint_states')]
     ])
