41fa0e7165a3f09e55689acdbce2c604743b4e28
Fortuz <fortuz19@gmail.com>
2025-11-17 14:00:24 +0100

    remove debug messages - Csenge

--- mecanumbot_core/mecanumbot_core/mecanumbot_sensorproc_node.py
+++ mecanumbot_core/mecanumbot_core/mecanumbot_sensorproc_node.py
@@ -106,9 +106,9 @@ class Mecanumbot_Sensorproc_Node(Node):
             msg.twist.twist.linear.x = Vx_tick * self.scale  # m/s
             msg.twist.twist.linear.y = Vy_tick * self.scale # m/s
             msg.twist.twist.angular.z = Wz_tick * self.scale / self.wheel_dist_scale  # rad/s
-            self.get_logger().info(f'Wheel ticks: BL: {self.cr_state.vel_bl}, BR: {self.cr_state.vel_br}, FL: {self.cr_state.vel_fl}, FR: {self.cr_state.vel_fr}')
-            self.get_logger().info(f'Wheel tick Velocities: Vx_tick: {Vx_tick}, Vy_tick: {Vy_tick}, Wz_tick: {Wz_tick}, scale: {self.scale}, wheel_dist_scale: {self.wheel_dist_scale}')
-            self.get_logger().info(f'Calculated Velocities: Vx: {msg.twist.twist.linear.x}, Vy: {msg.twist.twist.linear.y}, Wz: {msg.twist.twist.angular.z}')
+            #self.get_logger().info(f'Wheel ticks: BL: {self.cr_state.vel_bl}, BR: {self.cr_state.vel_br}, FL: {self.cr_state.vel_fl}, FR: {self.cr_state.vel_fr}')
+            #self.get_logger().info(f'Wheel tick Velocities: Vx_tick: {Vx_tick}, Vy_tick: {Vy_tick}, Wz_tick: {Wz_tick}, scale: {self.scale}, wheel_dist_scale: {self.wheel_dist_scale}')
+            #self.get_logger().info(f'Calculated Velocities: Vx: {msg.twist.twist.linear.x}, Vy: {msg.twist.twist.linear.y}, Wz: {msg.twist.twist.angular.z}')
             
             dx =  msg.twist.twist.linear.x * self.dt
             dy = msg.twist.twist.linear.y * self.dt
@@ -117,8 +117,8 @@ class Mecanumbot_Sensorproc_Node(Node):
             msg.pose.pose.position.x = self.odom.pose.pose.position.x + (math.cos(self.odom.pose.pose.orientation.z) * dx - math.sin(self.odom.pose.pose.orientation.z) * dy)
             msg.pose.pose.position.y = self.odom.pose.pose.position.y + (math.sin(self.odom.pose.pose.orientation.z) * dx + math.cos(self.odom.pose.pose.orientation.z) * dy)
             msg.pose.pose.position.z = 0.0
-            self.get_logger().info(f'Publishing: dx: {dx}, dy: {dy}, dtheta: {dtheta}')
-            self.get_logger().info(f'Current Odom: x: {self.odom.pose.pose.position.x}, y: {self.odom.pose.pose.position.y}, theta: {self.odom.pose.pose.orientation.z}')
+            #self.get_logger().info(f'Publishing: dx: {dx}, dy: {dy}, dtheta: {dtheta}')
+            #self.get_logger().info(f'Current Odom: x: {self.odom.pose.pose.position.x}, y: {self.odom.pose.pose.position.y}, theta: {self.odom.pose.pose.orientation.z}')
             if self.odom_from_imu: #TODO
                 # Orientation from IMU
                 msg.pose.pose.orientation.x = 0.0
