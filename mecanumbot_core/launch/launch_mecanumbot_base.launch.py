ae6ec56a71c2fdeb43a8af77d1b59a91ecaf801e
Fortuz <fortuz19@gmail.com>
2025-11-17 14:41:50 +0100

    fix namespace syntax

--- mecanumbot_core/launch/launch_mecanumbot_base.launch.py
+++ mecanumbot_core/launch/launch_mecanumbot_base.launch.py
@@ -38,7 +38,7 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        declare_namespace,
+        namespace,
         declare_sim_time,
 
         # mecanumbot_core IO node
@@ -46,7 +46,7 @@ def generate_launch_description():
             package='mecanumbot_core',
             executable='mecanumbot_io_node',
             name='mecanumbot_io_node',
-            namespace=declare_namespace,
+            namespace= namespace,
             parameters=[yaml_file, {'use_sim_time': use_sim_time}],
             output='screen'
         ),
@@ -56,7 +56,7 @@ def generate_launch_description():
             package='mecanumbot_core',
             executable='mecanumbot_sensorproc_node',
             name='mecanumbot_sensorproc_node',
-            namespace=declare_namespace,
+            namespace= namespace,
             parameters=[yaml_file, {'use_sim_time': use_sim_time}],
             output='screen'
         ),
@@ -66,7 +66,7 @@ def generate_launch_description():
             package='ld08_driver',
             executable='ld08_driver',
             name='ld08_driver_node',
-            namespace=declare_namespace,
+            namespace= namespace,
             parameters=[
                 {'port': '/dev/ttyUSB0'},
                 {'frame_id': 'laser_frame'},
@@ -80,7 +80,7 @@ def generate_launch_description():
             PythonLaunchDescriptionSource(state_publisher_path),
             launch_arguments={
                 'use_sim_time': use_sim_time,
-                'namespace': declare_namespace
+                'namespace': namespace
             }.items()
         )
     ])
