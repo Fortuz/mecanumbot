c9f6f8f67c091ec4b02bdaa275b9512496e69883
Fortuz <fortuz19@gmail.com>
2025-11-18 15:00:25 +0100

    trying to obtain namespace info

--- mecanumbot_core/mecanumbot_core/mecanumbot_sensorproc_node.py
+++ mecanumbot_core/mecanumbot_core/mecanumbot_sensorproc_node.py
@@ -49,7 +49,7 @@ class Mecanumbot_Sensorproc_Node(Node):
             self.odom_frame_id = self.namespace + '/' + self.odom_frame_id
             self.odom_child_frame_id = self.namespace + '/' + self.odom_child_frame_id
             self.imu_frame_id = self.namespace + '/' + self.imu_frame_id
-        self.get_logger().info(f'Namespace: {self.namespace}')
+        self.get_logger().info(f'Namespace: {namespace}')
         self.get_logger().info(f'Odom Frame ID: {self.odom_frame_id}, Child Frame ID: {self.odom_child_frame_id}, IMU Frame ID: {self.imu_frame_id}')
          # Robot parameters
         self.vel_tick = self.get_parameter('robot_params.wheel.vel_tick').value/60 # rot/min to rot/s
