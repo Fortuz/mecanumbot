b1293743ccb9edfec191666994d0c9f9e0e1e5bd
Fortuz <fortuz19@gmail.com>
2025-11-18 14:43:10 +0100

    fix syntax

--- mecanumbot_core/launch/launch_mecanumbot_base.launch.py
+++ mecanumbot_core/launch/launch_mecanumbot_base.launch.py
@@ -71,7 +71,7 @@ def generate_launch_description():
             namespace= namespace,
             parameters=[
                 {'port': '/dev/ttyUSB0'},
-                {'frame_id': [namespace, 'base_scan']},
+                {'frame_id': [namespace, '/base_scan']},
                 {'use_sim_time': use_sim_time}
             ],
             output='screen'
