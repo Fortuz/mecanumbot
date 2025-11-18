62d87649e89420addce5f05d206b629267917e02
Fortuz <fortuz19@gmail.com>
2025-11-18 14:17:50 +0100

    fix syntax error

--- mecanumbot_core/mecanumbot_core/mecanumbot_sensorproc_node.py
+++ mecanumbot_core/mecanumbot_core/mecanumbot_sensorproc_node.py
@@ -36,12 +36,14 @@ class Mecanumbot_Sensorproc_Node(Node):
          ])
         
         self.tf_broadcaster = TransformBroadcaster(self)
+        self.namespace = namespace
 
+         # Odom parameters
         self.odom_from_imu = self.get_parameter('odom_params.from_imu').value
         self.odom_frame_id = self.get_parameter('odom_params.frame_id').value
         self.odom_child_frame_id = self.get_parameter('odom_params.child_frame_id').value
         self.imu_frame_id = self.get_parameter('imu_params.frame_id').value
-        
+
         if self.namespace != '':
             self.odom_frame_id = self.namespace + '/' + self.odom_frame_id
             self.odom_child_frame_id = self.namespace + '/' + self.odom_child_frame_id
