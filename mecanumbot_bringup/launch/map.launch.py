f6ce6f2c8c89c38d31a54907afb7cfb6c6ba9a11
Csenge <csengehubay@gmail.com>
2025-12-08 15:11:51 +0100

    remove namespace from tf topics

--- mecanumbot_bringup/launch/map.launch.py
+++ mecanumbot_bringup/launch/map.launch.py
@@ -7,7 +7,7 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import PushRosNamespace
 
 def generate_launch_description():
-    map_name = 'AI_room_lounge'
+    map_name = 'ethodept_old'
     mecanumbot_description_pkg_share = get_package_share_directory('mecanumbot_description')
     param_file = os.path.join(mecanumbot_description_pkg_share, 'param', 'mecanumbot_custom_nav2.yaml')
     map_file = os.path.join(mecanumbot_description_pkg_share,'maps',map_name, f"{map_name}.yaml")
