b2ebf86dbd7adc2a749cc1e283ba8008a6cfc8f9
Csenge <csengehubay@gmail.com>
2026-04-09 14:47:24 +0200

    updated gripper posis

--- mecanumbot_core/mecanumbot_core/mecanumbot_IO_node.py
+++ mecanumbot_core/mecanumbot_core/mecanumbot_IO_node.py
@@ -112,7 +112,7 @@ class Mecanumbot_IO_Node(Node):
         
 
         self.cmd_outputs = {'BL_vel':0,'BR_vel':0,'FL_vel':0,'FR_vel':0,
-                            'N_pos':self.neck_default,'GL_pos':3.36,'GR_pos':6.83}
+                            'N_pos':self.neck_default,'GL_pos':683,'GR_pos':336}
         self.vel_subscription = self.create_subscription(Twist,
                                                          'cmd_vel', 
                                                          self.vel_cmd_callback, 
