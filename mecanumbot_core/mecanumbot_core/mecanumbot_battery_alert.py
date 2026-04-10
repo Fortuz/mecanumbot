9cd9b1a57bce8ead4d3a085879ce7217747c025f
Csenge <csengehubay@gmail.com>
2026-04-10 13:29:13 +0200

    fix battery

--- mecanumbot_core/mecanumbot_core/mecanumbot_battery_alert.py
+++ mecanumbot_core/mecanumbot_core/mecanumbot_battery_alert.py
@@ -23,7 +23,7 @@ class MecanumbotBatteryAlert(Node):
         self.timer = self.create_timer(timer_period, self.timer_callback, callback_group=self.callback_group)
         self.srv_client = self.create_client(SetLedStatus,'set_led_status')
 
-        self.battery_threshold = self.get_parameter('battery_threshold').get_parameter_value().double_value
+        self.battery_threshold = 9.7
 
         self.cr_battery_subscription = self.create_subscription(
             BatteryState,
@@ -69,6 +69,8 @@ class MecanumbotBatteryAlert(Node):
         if voltage < self.battery_threshold:
             self.get_logger().warn(f"{battery} battery low: {voltage:.2f} V")
             self.alert = True
+        else:
+            self.alert = False
 
 def main(args=None):
     rclpy.init(args=args)
