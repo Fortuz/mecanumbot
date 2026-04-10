38a9fea91cb9264c53733b06a6231817f1731b2a
Csenge <csengehubay@gmail.com>
2026-04-10 10:22:25 +0200

    minor bugfix

--- mecanumbot_core/mecanumbot_core/mecanumbot_battery_alert.py
+++ mecanumbot_core/mecanumbot_core/mecanumbot_battery_alert.py
@@ -20,7 +20,7 @@ class MecanumbotBatteryAlert(Node):
         timer_period = 1  # seconds
         self.callback_group = ReentrantCallbackGroup()
         self.timer = self.create_timer(timer_period, self.timer_callback, callback_group=self.callback_group)
-        self.srv_client = self.node.create_client(SetLedStatus,'/set_led_status')
+        self.srv_client = self.create_client(SetLedStatus,'set_led_status')
 
         self.battery_threshold = self.get_parameter('battery_threshold').get_parameter_value().double_value
 
