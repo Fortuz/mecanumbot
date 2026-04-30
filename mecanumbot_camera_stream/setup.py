9af4f47c6683cdfba4613e177dbc6b9b28b2571d
Csenge <csengehubay@gmail.com>
2026-04-30 12:26:07 +0200

    minimal camera publisher for raspi5

--- mecanumbot_camera_stream/setup.py
+++ mecanumbot_camera_stream/setup.py
@@ -30,6 +30,7 @@ setup(
             'camera_image_publisher_node = mecanumbot_camera_stream.camera_image_publisher_node:main',
             'compressed_camera_publisher_node = mecanumbot_camera_stream.compressed_camera_publisher_node:main',
             'h264_camera_publisher_node = mecanumbot_camera_stream.h264_camera_publisher_node:main',
+            'raspi_camnode = mecanumbot_camera_stream.raspi_camnode:main',
         ],
     },
 )
