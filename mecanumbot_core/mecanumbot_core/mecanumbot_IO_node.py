1e9469ac08ea2273f7355f376a7c9e389777141b
Csenge <csengehubay@gmail.com>
2026-06-29 09:54:22 +0200

    add debug neck line

--- mecanumbot_core/mecanumbot_core/mecanumbot_IO_node.py
+++ mecanumbot_core/mecanumbot_core/mecanumbot_IO_node.py
@@ -226,6 +226,7 @@ class Mecanumbot_IO_Node(Node):
         self.opencr_state.acc_fl = shorts[18]
         self.opencr_state.acc_fr = shorts[19]
         #self.opencr_state.pos_n = shorts[20]
+        self.get_logger().info(f"Received neck position from OpenCR: {shorts[20]}")
         self.opencr_state.pos_n = int(self.cmd_outputs['N_pos'])  # Use the commanded neck position instead of the received value
         self.opencr_state.pos_gl = shorts[21]
         self.opencr_state.pos_gr = shorts[22]
