2f4d93660bb99bcd366f3702cc93bcf4cdd9e41a
Csenge <csengehubay@gmail.com>
2026-06-29 11:10:58 +0200

    neck pos feedback fixed?

--- mecanumbot_core/mecanumbot_core/mecanumbot_IO_node.py
+++ mecanumbot_core/mecanumbot_core/mecanumbot_IO_node.py
@@ -225,10 +225,7 @@ class Mecanumbot_IO_Node(Node):
         self.opencr_state.acc_br = shorts[17]
         self.opencr_state.acc_fl = shorts[18]
         self.opencr_state.acc_fr = shorts[19]
-        #self.opencr_state.pos_n = shorts[20]
-        self.get_logger().info(f"Received neck position from OpenCR: {shorts[20]}, gripper left: {shorts[21]}, gripper right: {shorts[22]}")
-        self.opencr_state.pos_n = int(self.cmd_outputs['N_pos'])  # Use the commanded neck position instead of the received value
-        self.opencr_state.pos_gl = shorts[21]
+        self.opencr_state.pos_n = shorts[20]
         self.opencr_state.pos_gr = shorts[22]
         self.opencr_state.err_bl = shorts[23]
         self.opencr_state.err_br = shorts[24]
