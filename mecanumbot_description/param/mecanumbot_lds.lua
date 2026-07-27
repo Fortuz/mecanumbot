334f8b3690bab71d1389cac04be467a0b0a7d38b
Csenge <csengehubay@gmail.com>
2026-07-27 11:10:16 +0200

    fix attempt 2

--- mecanumbot_description/param/mecanumbot_lds.lua
+++ mecanumbot_description/param/mecanumbot_lds.lua
@@ -16,7 +16,7 @@ options = {
   
   provide_odom_frame = false,
   publish_frame_projected_to_2d = true,
-  use_odometry = false,
+  use_odometry = true,
   use_nav_sat = false,
   use_landmarks = false,
   num_laser_scans = 1,
@@ -30,7 +30,7 @@ options = {
   pose_publish_period_sec = 5e-2,
   trajectory_publish_period_sec = 30e-3,
   rangefinder_sampling_ratio = 1.,
-  odometry_sampling_ratio = 1.,
+  odometry_sampling_ratio = 0.5,
   fixed_frame_pose_sampling_ratio = 1.,
   imu_sampling_ratio = 1.,
   landmarks_sampling_ratio = 1.,
